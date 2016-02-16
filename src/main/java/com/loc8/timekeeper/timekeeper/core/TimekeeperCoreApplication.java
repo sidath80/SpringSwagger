bec5ad6a113bba3fbc9a67e6e51202f2350dbbb8
Sidath Dassanayake <sdassanayake@loc8.com>
2016-02-17 10:54:44 +1100

    Add Debug Print

--- src/main/java/com/loc8/timekeeper/timekeeper/core/TimekeeperCoreApplication.java
+++ src/main/java/com/loc8/timekeeper/timekeeper/core/TimekeeperCoreApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class TimekeeperCoreApplication {
 
 	public static void main(String[] args) {
+		System.out.println("Debug 1");
 		SpringApplication.run(TimekeeperCoreApplication.class, args);
 	}
 }
