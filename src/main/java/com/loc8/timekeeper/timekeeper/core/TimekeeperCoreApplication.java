6b772bb9879b2aae54cfe21bc50587875ea0b102
Sidath Dassanayake <sdassanayake@loc8.com>
2016-02-17 10:41:30 +1100

    Initial Commit

---
+++ src/main/java/com/loc8/timekeeper/timekeeper/core/TimekeeperCoreApplication.java
@@ -0,0 +1,12 @@
+package com.loc8.timekeeper.timekeeper.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimekeeperCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TimekeeperCoreApplication.class, args);
+	}
+}
